d8984744557d524b8c04b9363f4367bbd5f9d169
Shola Ayeni <ayenisholah@yahoo.com>
2024-11-11 21:21:37 +0100

    cache balance

--- src/utils/balance.ts
+++ src/utils/balance.ts
@@ -1,11 +1,19 @@
 import { ethers } from "ethers";
 import { axiosInstance, limiter } from "../init";
+import redisClient from "./redis";
 const API_KEY = process.env.API_KEY
 
+const redis = redisClient.getClient()
 
 const provider = new ethers.providers.AlchemyProvider("mainnet", "0rk2kbu11E5PDyaUqX1JjrNKwG7s4ty5")
 export async function getWethBalance(address: string): Promise<number> {
   try {
+    const cacheKey = `weth_balance:${address}`;
+    const cachedBalance = await redis.get(cacheKey);
+    if (cachedBalance) {
+      return Number(cachedBalance);
+    }
+
     const payload = {
       "id": "WalletPopoverDataPollerClosedQuery",
       "query": "query WalletPopoverDataPollerClosedQuery(\n  $address: AddressScalar!\n  $wrappedCurrencySymbol: String!\n  $wrappedCurrencyChain: ChainScalar!\n) {\n  ...WalletAndAccountButtonFundsDisplay_data_p0g3U\n}\n\nfragment FundsDisplay_walletFunds on WalletFundsType {\n  symbol\n  quantity\n}\n\nfragment WalletAndAccountButtonFundsDisplay_data_p0g3U on Query {\n  wallet(address: $address) {\n    wrappedCurrencyFunds: fundsOf(symbol: $wrappedCurrencySymbol, chain: $wrappedCurrencyChain) {\n      quantity\n      symbol\n      ...FundsDisplay_walletFunds\n      id\n    }\n  }\n}\n",
@@ -28,7 +36,11 @@ export async function getWethBalance(address: string): Promise<number> {
       }
     ));
 
-    return Number(data.data.wallet.wrappedCurrencyFunds.quantity);
+    const balance = Number(data.data.wallet.wrappedCurrencyFunds.quantity);
+
+    await redis.set(cacheKey, balance.toString(), 'EX', 60);
+
+    return balance;
   } catch (error) {
     console.error("Error fetching WETH balance:", error);
     return 0
@@ -37,14 +49,21 @@ export async function getWethBalance(address: string): Promise<number> {
 
 export async function getBethBalance(address: string): Promise<number> {
   try {
+    const cacheKey = `beth_balance:${address}`;
+    const cachedBalance = await redis.get(cacheKey);
+    if (cachedBalance) {
+      return Number(cachedBalance);
+    }
+
     const BLUR_POOL_ADDRESS = "0x0000000000A39bb272e79075ade125fd351887Ac";
     const wethContract = new ethers.Contract(BLUR_POOL_ADDRESS, ['function balanceOf(address) view returns (uint256)'], provider);
     const balance = await wethContract.balanceOf(address);
-    return Number(ethers.utils.formatEther(balance));
+    const formattedBalance = Number(ethers.utils.formatEther(balance));
+    await redis.set(cacheKey, formattedBalance.toString(), 'EX', 60);
+    return formattedBalance;
   } catch (error) {
     console.error("Error fetching BETH balance:", error);
     return 0
-    // throw error; // Rethrow the error after logging
   }
 }
 
